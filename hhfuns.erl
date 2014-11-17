64316d069cd4aece685f27c1e38b775881f69765
Han Seung Hyun <han1448oppa@naver.com>
2014-11-17 16:54:46 +0900

    changed line

--- hhfuns.erl
+++ hhfuns.erl
@@ -61,8 +61,7 @@ max2([_|T],Max) -> max2(T, Max).
 
 min([H|T]) -> min2(T,H).
 min2([],Min) -> Min;
-min2([H|T], Min) when H < Min ->
-	min2(T,H);
+min2([H|T], Min) when H < Min -> min2(T,H);
 min2([_|T], Min) -> min2(T,Min).
 
 
