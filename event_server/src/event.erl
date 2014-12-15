9a127cfe8750d9d94ce311c50f7805f9c59fad7e
Han Seung Hyun <han1448oppa@naver.com>
2014-12-15 23:38:03 +0900

    event

---
+++ event_server/src/event.erl
@@ -0,0 +1,12 @@
+-module(event).
+-complie([loop/1]).
+-record(state, {server, name="", to_go=0}).
+
+
+loop (S = #state{server=Server}) ->
+	receive
+		{Server, Ref, cancel} ->
+			Server ! {Ref, ok}
+	after S#state.to_go*1000 ->
+		Server ! {done, S#state.name}
+	end.
