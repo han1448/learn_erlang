1426c330dc3e6ef6db1e4b92831dfe132f1faed8
Han Seung Hyun <han1448oppa@naver.com>
2014-12-15 20:45:05 +0900

    end linkmon

--- linkmon.erl
+++ linkmon.erl
@@ -47,7 +47,7 @@ start_critic2() ->
 
 restarter() ->
 	process_flag(trap_exit, true),
-	Pid = spawn_link(?MODULE, critic, []),
+	Pid = spawn_link(?MODULE, critic2, []),
 	register(critic, Pid),
 	receive
 		{'EXIT', Pid, mormal} ->
@@ -62,14 +62,24 @@ restarter() ->
 judge2(Band, Album) ->
 	Ref = make_ref(),
 	critic ! {self(),Ref, {Band, Album}},
-	Pid = whereis(critic),
 	receive
 		{Ref, Criticism} -> Criticism
 	after 2000 ->
 		timeout
 	end.
 
-
+critic2() ->
+	receive
+		{From, Ref, {"Rage Against the Turing Machine", "Unit Testify"}} ->
+			From ! {Ref, "They are great!"};
+		{From, Ref, {"System of a Downtime", "Memoize"}} ->
+			From ! {Ref, "They're not Johnny Crash but they're good."};
+		{From, Ref, {"Johnny Crash", "The Token Ring of Fire"}} ->
+			From ! {Ref, "Simply incredible."};
+		{From, Ref, {_Band, _Album}} ->
+			From ! {Ref, "They are terrible!"}
+	end,
+critic2().
 
 
 
