345e5cce036b2ee6e294f122aee47cb6c59c2464
Han Seung Hyun <han1448oppa@naver.com>
2015-01-10 17:48:13 +0900

    start otp

---
+++ event_server/src/kitty_server.erl
@@ -0,0 +1,7 @@
+-module(kitty_server).
+-export([start_link/0, order_cat/4, return_cat/2, close_shop/1]).
+-record(cat, {name, color=green, description}).
+
+
+%%% Client API
+start_link() -> spawn_link(fun init/0).
